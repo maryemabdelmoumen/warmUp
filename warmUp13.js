4be47fd46f5389f182d8b023e4b400f3c5220e1b
maryemabdelmoumen <abdelmoumenmaryem5@gmail.com>
2024-01-18 13:12:08 +0100

    warmUp13 done,day4

--- warmUp13.js
+++ warmUp13.js
@@ -10,5 +10,20 @@
  * @return {[]}
  */
 const intDiff = function (arr, n) {
-    // your code goes here
-};
+    if (arr.length < 2) {
+        return 0
+    }
+    var count = 0
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            // Check if the difference between the pair is equal to n
+            if (Math.abs(arr[i] - arr[j]) === n) {
+                // Increment the count if the difference is equal to n
+                count++
+            }
+        }
+    }
+    return count
+}
+
+    
